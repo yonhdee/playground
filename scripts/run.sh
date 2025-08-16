5e60d3da0d805151f8e79493051a19624f55fec8
yonhdee <yonh_dee@hotmail.com>
2025-08-16 23:22:26 +0800

    update docker & scripts

---
+++ scripts/run.sh
@@ -0,0 +1,5 @@
+#/bin/bash
+
+source ./install/setup.bash
+
+ros2 launch robot_description visualize.launch.py
--- scripts/start_gui.sh
+++ scripts/start_gui.sh
@@ -12,9 +12,12 @@ docker run -itd --name ros2_playground \
     --rm \
     --network=host \
     --privileged \
-    -e DISPLAY \
+    -e DISPLAY=$DISPLAY \
+    -e XDG_RUNTIME_DIR=/tmp/runtime-$UID \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
     -v $HOME/.Xauthority:/root/.Xauthority \
     -v /dev/input:/dev/input:rw \
+    -v /dev/dri:/dev/dri \
     -v $CURRENT_DIR:/root/ros2_ws:rw \
     -w /root/ros2_ws \
     ros2_playground:latest
